7c4937002226d09ba2243b69651942f36b213dd0
1seok2 <misohaha31@naver.com>
2021-04-25 21:05:40 +0900

    feat: netlify deploying

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,3 +1,8 @@
+/**
+ * @description 어플리케이션의 헤더
+ *              path에 따라 커스텀된 헤더 반환
+ */
+
 import React from 'react';
 import { AiOutlineBell, AiOutlineQrcode } from 'react-icons/ai';
 import { HeaderType } from '@interfaces/app';
