4acae67e30a15bc7618c6ee7fa9f0701110e84d1
1seok2 <misohaha31@naver.com>
2021-04-26 23:53:05 +0900

    test: remove header option

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,25 +5,27 @@
 
 import React from 'react';
 import { AiOutlineBell, AiOutlineQrcode } from 'react-icons/ai';
-import { HeaderType } from '@interfaces/app';
+// import { HeaderType } from '@interfaces/app';
 import { HeaderContainer } from './header.styled';
 import { theme } from '@components/utils/styles/theme';
 
-const Header = ({ pathname }: HeaderType) => {
-  const isMain = pathname === '/';
+const Header = () =>
+  // { pathname }: HeaderType
+  {
+    // const isMain = pathname === '/';
 
-  return (
-    <HeaderContainer isMain={isMain}>
-      <AiOutlineQrcode
-        size={theme.icon.fontSize}
-        style={{ color: theme.color.white, marginRight: '12px' }}
-      />
-      <AiOutlineBell
-        size={theme.icon.fontSize}
-        style={{ color: theme.color.white }}
-      />
-    </HeaderContainer>
-  );
-};
+    return (
+      <HeaderContainer isMain={true}>
+        <AiOutlineQrcode
+          size={theme.icon.fontSize}
+          style={{ color: theme.color.white, marginRight: '12px' }}
+        />
+        <AiOutlineBell
+          size={theme.icon.fontSize}
+          style={{ color: theme.color.white }}
+        />
+      </HeaderContainer>
+    );
+  };
 
 export default Header;
