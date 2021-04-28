4c20f6f62137e529c47aa02af2bcb8a395e395de
1seok2 <misohaha31@naver.com>
2021-04-28 22:32:08 +0900

    test: renew

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,39 @@
+import { Container } from 'next/app';
+import Head from 'next/head';
+import React from 'react';
+
 import { OuterContainer } from '@components/utils/styles/container.styled';
 import { AppType } from '@interfaces/app';
 
 import '@assets/css/app.css';
 import '@assets/css/scroll.css';
+
 import Header from '@components/header';
 
-const App = ({
-  Component,
-  pageProps,
-}: // router: { pathname }
-AppType) => {
+const App = ({ Component, pageProps }: AppType) => {
   return (
-    <>
-      <Header />
-      <OuterContainer>
-        <Component {...pageProps} />
-      </OuterContainer>
-    </>
+    <Container>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
+      <div style={styles.container}>
+        <Header />
+        <OuterContainer>
+          <Component {...pageProps} />
+        </OuterContainer>
+      </div>
+    </Container>
   );
 };
 
 export default App;
+
+const styles = {
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+};
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,33 +8,20 @@ import { AiOutlineBell, AiOutlineQrcode } from 'react-icons/ai';
 // import { HeaderType } from '@interfaces/app';
 import { HeaderContainer } from './header.styled';
 import { theme } from '@components/utils/styles/theme';
-import Head from 'next/head';
 
-const Header = () =>
-  // { pathname }: HeaderType
-  {
-    // const isMain = pathname === '/';
-
-    return (
-      <>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-          />
-        </Head>
-        <HeaderContainer>
-          <AiOutlineQrcode
-            size={theme.icon.fontSize}
-            style={{ color: theme.color.white, marginRight: '12px' }}
-          />
-          <AiOutlineBell
-            size={theme.icon.fontSize}
-            style={{ color: theme.color.white }}
-          />
-        </HeaderContainer>
-      </>
-    );
-  };
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <AiOutlineQrcode
+        size={theme.icon.fontSize}
+        style={{ color: theme.color.white, marginRight: '12px' }}
+      />
+      <AiOutlineBell
+        size={theme.icon.fontSize}
+        style={{ color: theme.color.white }}
+      />
+    </HeaderContainer>
+  );
+};
 
 export default Header;
