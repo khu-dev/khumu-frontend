5f501445a737f53d7abc679712214836a19aae12
1seok2 <misohaha31@naver.com>
2021-05-04 21:00:54 +0900

    fix: checkbox style

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -2,14 +2,19 @@
 import { forwardRef } from 'react';
 import { css } from '@emotion/react';
 
-const CheckboxStyle = css`
-  width: 13px;
-  height: 13px;
-  margin-right: 12px;
-`;
-
 const Checkbox = forwardRef<HTMLInputElement>(function Checkbox(props, ref) {
-  return <input css={CheckboxStyle} type="checkbox" ref={ref} {...props} />;
+  return (
+    <input
+      css={css`
+        width: 13px;
+        height: 13px;
+        margin-right: 12px;
+      `}
+      type="checkbox"
+      ref={ref}
+      {...props}
+    />
+  );
 });
 
 export default Checkbox;
