2de26ffd05fafc9a8fb33c992d858b2f2d75ec57
1seok2 <misohaha31@naver.com>
2021-05-21 00:15:20 +0900

    fix: deploy error

--- src/components/Skeleton/Main/Item.tsx
+++ src/components/Skeleton/Main/Item.tsx
@@ -1,17 +1,11 @@
-import React from 'react';
-import MainSkeletonStyleTag from './style';
-
 export default function SkeletonMainItem() {
   return (
-    <>
-      <MainSkeletonStyleTag />
-      <div className={'main-skeleton-container'}>
-        <div className={'skeleton-middle-container skeleton-container'}>
-          <div className={'skeleton skeleton-title-02'} />
-          <div className={'skeleton skeleton-description-01'} />
-          <div className={'skeleton skeleton-description-01'} />
-        </div>
+    <div className={'main-skeleton-container'}>
+      <div className={'skeleton-middle-container skeleton-container'}>
+        <div className={'skeleton skeleton-title-02'} />
+        <div className={'skeleton skeleton-description-01'} />
+        <div className={'skeleton skeleton-description-01'} />
       </div>
-    </>
+    </div>
   );
 }
