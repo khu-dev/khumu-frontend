2f1eafe664f361d007268bf2a02b032335da373f
1seok2 <misohaha31@naver.com>
2021-05-20 23:50:51 +0900

    fix: deploy error

--- src/api/api-qrcode.ts
+++ src/api/api-qrcode.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { BASE_URI } from '@config/baseURI';
 
-import { accesskey } from '@config/key';
+import { accesskey } from '@config/_key';
+// import { accesskey } from '@config/key';
 
 export const fetchQRCode = (token: string = accesskey) =>
   axios.get(BASE_URI + '/users/me/qr-code', {
