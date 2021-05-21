80556309430ff54b3e3bd3a02fccee6625d3076a
1seok2 <misohaha31@naver.com>
2021-05-21 23:40:37 +0900

    fix: qrcode error about access token

--- pages/qrcode.tsx
+++ pages/qrcode.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 import Qrcode from '@views/Qrcode';
 import QrcodeHeader from '@components/Header/Qrcode';
-import { GetServerSideProps } from 'next';
-import { fetchToken } from '@api/api-token';
-import { fetchQRCode } from '@api/api-qrcode';
-import { ID, PW } from '@config/_key';
+import { exampleData } from '@constants/data';
+// import { GetServerSideProps } from 'next';
+// import { fetchToken } from '@api/api-token';
+// import { fetchQRCode } from '@api/api-qrcode';
+// import { ID, PW } from '@config/_key';
 // import { ID, PW } from '@config/key';
 
 interface QRCodePageProps {
@@ -15,10 +16,17 @@ interface QRCodePageProps {
   department: string;
 }
 
+const initial = {
+  qr_code_str: exampleData,
+  name: '가나다',
+  student_number: '2039182031',
+  department: '라마바사아',
+};
+
 export default function QRCodePage({
   qr_code_str,
   ...profile
-}: QRCodePageProps) {
+}: QRCodePageProps = initial) {
   return (
     <>
       <QrcodeHeader title={'모바일 이용증'} />
@@ -27,21 +35,21 @@ export default function QRCodePage({
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const {
-    data: { access },
-  }: any = await fetchToken({
-    // username: 'username',
-    username: ID,
-    // password: 'password',
-    password: PW,
-  });
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const {
+//     data: { access },
+//   }: any = await fetchToken({
+//     // username: 'username',
+//     username: ID,
+//     // password: 'password',
+//     password: PW,
+//   });
 
-  const {
-    data: { data },
-  } = await fetchQRCode(access);
+//   const {
+//     data: { data },
+//   } = await fetchQRCode(access);
 
-  return {
-    props: data,
-  };
-};
+//   return {
+//     props: data,
+//   };
+// };
