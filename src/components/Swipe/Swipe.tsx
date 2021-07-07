0698559745c9c6b9d26a4616bc21ca09b3960938
1seok2 <misohaha31@naver.com>
2021-07-07 23:08:08 +0900

    feat: add swipe element

---
+++ src/components/Swipe/Swipe.tsx
@@ -0,0 +1,46 @@
+/**
+ * @description 제작된 페이지 목록
+ */
+import React from 'react';
+import { css } from '@emotion/react';
+import { useSwipeElement } from 'src/hooks/useSwipeElement';
+
+const threshold = 100;
+
+export default function Swipe({ children, ...rest }) {
+  const {
+    gap,
+    isMoving,
+    handler: { handleTouchStart, handleTouchMove, handleTouchEnd, isEvent },
+  } = useSwipeElement({ threshold, callback: () => console.log('event!') });
+
+  return (
+    <div
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+      css={css`
+        width: 100%;
+        height: 64px;
+        background-color: green;
+
+        ${isMoving
+          ? css`
+              transform: translate(-${gap}px);
+            `
+          : isEvent()
+          ? css`
+              transition: all 0.3s;
+              transform: translateX(-100%);
+            `
+          : css`
+              transition: all 0.4s;
+              transform: translateX(0);
+            `}
+      `}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+}
---
+++ src/hooks/useSwipeElement.ts
@@ -0,0 +1,56 @@
+import React from 'react';
+
+/**
+ * @param threshold 왼쪽으로 해당 값보다 더 움직이면 이벤트 발생
+ * @returns
+ */
+export const useSwipeElement = ({ threshold = 80, callback }) => {
+  const [isMoving, setMoving] = React.useState(false);
+  const [position, setPosition] = React.useState({
+    start: 0,
+    end: 0,
+  });
+
+  const handleTouchStart = (e) => {
+    setMoving(true);
+    setPosition({
+      start: e.targetTouches[0].clientX,
+      end: e.targetTouches[0].clientX,
+    });
+  };
+
+  const handleTouchMove = (e) => {
+    setPosition({
+      ...position,
+      end: e.targetTouches[0].clientX,
+    });
+  };
+
+  const handleTouchEnd = () => {
+    const gap = position.start - position.end;
+    setMoving(false);
+
+    if (gap > threshold) {
+      callback();
+    } else {
+      setTimeout(() => {
+        setPosition({
+          start: 0,
+          end: 0,
+        });
+      }, 500);
+    }
+  };
+
+  const isEvent = () => {
+    return position.start - position.end > threshold;
+  };
+
+  return {
+    startPoint: position.start,
+    endPoint: position.end,
+    gap: position.start - position.end,
+    isMoving,
+    handler: { handleTouchStart, handleTouchMove, handleTouchEnd, isEvent },
+  };
+};
