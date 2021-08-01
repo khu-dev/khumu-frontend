0b88c9861011851025aaa6278d843bef632c61e5
1seok2 <misohaha31@naver.com>
2021-08-01 21:03:24 +0900

    fix: change naming circles -> club

--- pages/circles.tsx
+++
@@ -1,5 +0,0 @@
-import Circles from '@views/Circles';
-
-const CirclesPage = () => <Circles />;
-
-export default CirclesPage;
---
+++ pages/club.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Circles from '@views/Club';
+
+const CirclesPage = () => (
+  <>
+    <Circles />
+  </>
+);
+
+export default CirclesPage;
