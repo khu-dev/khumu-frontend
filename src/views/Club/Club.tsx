d8cb89a6c1216b12dbc667001b9eec253c17e239
1seok2 <misohaha31@naver.com>
2021-08-01 23:44:25 +0900

    feat: set basic club frames

--- src/views/Club/Club.tsx
+++ src/views/Club/Club.tsx
@@ -1,3 +1,58 @@
-const Circles = () => <div>circles</div>;
+import React, { useState } from 'react';
+import { css } from '@emotion/react';
+import { IMG_URI } from '@config/baseURI';
+import { IoChevronBackOutline, IoChevronForwardOutline } from 'react-icons/io5';
+import * as s from './styled';
+
+const Circles = ({ clubs }) => {
+  const clubLength = clubs.length;
+  const [current, setCurrent] = useState(0);
+
+  const handleIndex = {
+    minus: () => {
+      current > 0 && setCurrent((prev) => (prev - 1) % clubLength);
+    },
+    plus: () => {
+      current < clubLength && setCurrent((prev) => (prev + 1) % clubLength);
+    },
+  };
+
+  const currentClub = clubs[current];
+  console.log(currentClub);
+
+  return (
+    <>
+      <s.Tag>전체</s.Tag>
+      <s.Card>
+        <s.Image url={`${IMG_URI}/${currentClub.images[0]}`} />
+        <s.Content>
+          <s.Tag
+            css={css`
+              margin: 0;
+            `}
+          >
+            연행
+          </s.Tag>
+          <s.NameWrapper>
+            <s.Name>{currentClub.name}</s.Name>
+            <s.Summary>{'태그'}</s.Summary>
+          </s.NameWrapper>
+          <s.P>{currentClub.description}</s.P>
+        </s.Content>
+      </s.Card>
+      <s.Paging>
+        <IoChevronBackOutline
+          onClick={handleIndex.minus}
+          css={current === 0 ? s.disabledStyle : s.enabledStyle}
+        />
+        <span>{`${current + 1} / ${clubLength}`}</span>
+        <IoChevronForwardOutline
+          onClick={handleIndex.plus}
+          css={current === clubLength ? s.disabledStyle : s.enabledStyle}
+        />
+      </s.Paging>
+    </>
+  );
+};
 
 export default Circles;
---
+++ src/views/Club/styled.ts
@@ -0,0 +1,91 @@
+import { color } from '@constants/theme';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+export const Tag = styled.div`
+  width: 53px;
+  height: 29px;
+  border-radius: 15px;
+  margin: 4px auto;
+  margin-bottom: 8px;
+  text-align: center;
+  line-height: 29px;
+  font-size: 16px;
+  font-weight: 500;
+  color: ${color.white};
+  background-color: ${color.main};
+`;
+
+export const Card = styled.div`
+  flex-grow: 1;
+  margin: 0 auto;
+  min-width: 260px;
+  width: calc(100% - 64px);
+  box-shadow: -1px 1px 12px -4px rgba(0, 0, 0, 0.3);
+  border: 1px solid ${color.main};
+  border-radius: 24px;
+  border-bottom-right-radius: 0;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: stretch;
+`;
+
+export const Image = styled.div<{ url: string }>`
+  flex: 1;
+  background-image: url(${({ url }) => url});
+  background-size: cover;
+  background-position: center;
+`;
+
+export const Content = styled.div`
+  height: 178px;
+  border-top: 1px solid ${color.gray4};
+  padding: 16px;
+`;
+
+export const NameWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-top: 6px;
+`;
+
+export const Name = styled.h2`
+  margin: 0;
+  margin-right: 16px;
+  color: ${color.main};
+`;
+
+export const Summary = styled.span`
+  color: ${color.black};
+`;
+
+export const P = styled.p`
+  margin-top: 8px;
+  font-size: 14px;
+  font-weight: 300;
+  color: ${color.gray1};
+`;
+
+export const Paging = styled.div`
+  padding: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+`;
+
+const buttonStyle = css`
+  font-size: 18px;
+`;
+
+export const disabledStyle = css`
+  ${buttonStyle};
+  color: ${color.gray4};
+`;
+
+export const enabledStyle = css`
+  ${buttonStyle};
+  color: ${color.main};
+`;
