6216059fe082ded780b453f090c7328c50496f65
Leon <misohaha31@naver.com>
2021-09-08 23:05:08 +0900

    refactor: 메인 피드백 타이틀 삭제

--- src/views/Main/Feed/FeedContainer.tsx
+++ src/views/Main/Feed/FeedContainer.tsx
@@ -44,7 +44,6 @@ const Feed = () => {
     <div className={'main-feed-container'}>
       <FeedInputProvider>
         <FeedInput>
-          <FeedInput.Title title={'쿠뮤에게 말해줘'} />
           <FeedInput.TextArea placeholder={'자유로운 피드백 부탁드립니다'} />
         </FeedInput>
       </FeedInputProvider>
