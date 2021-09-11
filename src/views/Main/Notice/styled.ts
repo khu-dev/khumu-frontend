48adb8f2cd4fd298552f67e8b5f91111dad6e572
Leon <leon@dunamu.com>
2021-09-12 00:41:50 +0900

    feat: add main notice

---
+++ src/views/Main/Notice/styled.ts
@@ -0,0 +1,60 @@
+import styled from '@emotion/styled';
+import { box, color } from '@constants/theme';
+import { css } from '@emotion/react';
+
+const textOverflowHiddenStyle = css`
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+`;
+
+export const NoticeItem = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin-bottom: 18px;
+`;
+
+export const NoticeThumbnail = styled.div`
+  display: inline-block;
+  width: 34px;
+  height: 34px;
+  border-radius: 50%;
+  background-color: ${color.main};
+  margin-right: 6px;
+`;
+
+export const NoticeBody = styled.div`
+  display: inline-block;
+  flex-grow: 1;
+  height: 48px;
+  border: 1px solid ${color.main};
+  border-radius: ${box.borderRadius};
+  border-top-left-radius: 0;
+  position: relative;
+  padding: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: stretch;
+`;
+
+export const Title = styled.h3`
+  font-weight: 600;
+`;
+
+export const Content = styled.p`
+  font-weight: 400;
+  width: calc(100% - 32px);
+  ${textOverflowHiddenStyle};
+`;
+
+export const More = styled.span`
+  position: absolute;
+  right: 8px;
+  bottom: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+`;
