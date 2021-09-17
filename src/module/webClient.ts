db03bda18ae0ebb5958fd902fbf72fedc42ad23f
Leon <leon@dunamu.com>
2021-09-18 01:09:38 +0900

    fix: resolve deploy error

--- src/module/webClient.ts
+++ src/module/webClient.ts
@@ -5,27 +5,30 @@ import { AndroidToast } from '@utils/android';
 
 const devToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN || accesskey;
 let _window;
-
-if (process.browser && typeof window !== undefined) _window = window as any;
-
-const token = _window?.Android?.getToken() || devToken;
-
-const webClient = axios.create({
+let token;
+let webClient = axios.create({
   baseURL: BASE_URI,
 });
 
+if (process.browser && typeof window !== undefined) {
+  _window = window as any;
+
+  token = _window?.Android?.getToken() || devToken;
+
+  webClient.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      console.log(error);
+
+      if (error.response?.status === 401 && error.config?.url !== '/logout') {
+        AndroidToast('로그인 페이지로 이동합니다');
+        _window.location.href = '/logout';
+      }
+
+      return Promise.reject(error);
+    },
+  );
+}
 webClient.defaults.headers['Authorization'] = `Bearer ${token}`;
-
-webClient.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    if (error.response?.status === 401 && error.config?.url !== '/logout') {
-      AndroidToast('로그인 페이지로 이동합니다');
-      _window.location.href = '/logout';
-    }
-
-    return Promise.reject(error);
-  },
-);
 
 export { webClient };
