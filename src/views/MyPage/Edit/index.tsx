500f2a58df13f7a239e3d94e1d84a78fa9a1b63c
Leon <leon@dunamu.com>
2021-12-30 00:50:41 +0900

    기능별 분리

--- src/views/MyPage/Edit/index.tsx
+++ src/views/MyPage/Edit/index.tsx
@@ -1,12 +1,9 @@
 import { useState, useEffect } from 'react'
 
 import { UserApi } from '@api/UserApi'
-import { DepartmentApi } from '@api/DepartmentApi'
-import { useToken } from '@context/Token'
 import { useUser } from '@context/User'
 import { AndroidToast } from '@utils/android'
 
-import { Department as DepartmentType } from '@interface/Department'
 import { Edit as EditType } from '@interface/User'
 
 import Button from './Button'
@@ -18,18 +15,8 @@ import StudentNumber from './StudentNumber'
 import * as s from './styled'
 
 const Edit = () => {
-  const { token } = useToken()
-  const {
-    user: { username, department, nickname, student_number, profile_image },
-    updateUser,
-  } = useUser()
-
-  const [departments, setDepartments] = useState<DepartmentType[]>([])
-  const [value, setValue] = useState<EditType>({
-    nickname: nickname || '',
-    department,
-    profile_image,
-  })
+  const { user, updateUser } = useUser()
+  const [value, setValue] = useState<EditType>(user)
 
   const handleChange = (name: string, value: string) => {
     setValue((prev) => ({
@@ -56,40 +43,15 @@ const Edit = () => {
   }
 
   useEffect(() => {
-    setValue({
-      nickname: nickname || '',
-      department,
-      profile_image,
-    })
-  }, [nickname, username, department, profile_image])
-
-  useEffect(() => {
-    if (!token) return
-
-    DepartmentApi.query().then(({ status, data }) => {
-      if (status !== 200) {
-        AndroidToast('학과를 불러오는데 실패하였습니다')
-        return
-      }
-
-      const newData = data.map((info: DepartmentType) => ({
-        name: info.name,
-        id: info.id,
-      }))
-      setDepartments(newData)
-    })
-  }, [token])
+    setValue(user)
+  }, [user])
 
   return (
     <s.Container>
       <Image onChange={handleChange} />
       <Nickname value={value.nickname!} onChange={handleChange} />
-      <StudentNumber studentNumber={student_number!} />
-      <Department
-        value={value.department!}
-        departments={departments}
-        onChange={handleChange}
-      />
+      <StudentNumber />
+      <Department value={value.department!} onChange={handleChange} />
       <Button onClick={handleSubmit} />
     </s.Container>
   )
