5c2932842cc38344932b94cecb8fa9e1f62ea631
Leon <leon@dunamu.com>
2021-12-30 00:41:06 +0900

    기능별 분리

--- pages/mypage/edit.tsx
+++ pages/mypage/edit.tsx
@@ -1,77 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useRouter } from 'next/router'
 
-import { UserApi } from '@api/UserApi'
-import { DepartmentApi } from '@api/DepartmentApi'
-import { useToken } from '@context/Token'
-import { useUser } from '@context/User'
-import { AndroidToast } from '@utils/android'
-
-import { Department } from '@interface/Department'
-import { Edit as EditType } from '@interface/User'
-
-import Edit from '@views/MyPage/Edit'
 import CommonHeader from '@components/Header/Common'
+import Edit from '@views/MyPage/Edit'
 
 export default function MyEditPage() {
-  const { token } = useToken()
-  const {
-    user: { username, department, nickname, student_number, profile_image },
-    updateUser,
-  } = useUser()
   const router = useRouter()
-  const [departments, setDepartments] = useState<Department[]>([])
-  const [value, setValue] = useState<EditType>({
-    nickname: nickname || '',
-    department,
-    profile_image,
-  })
-
-  const handleChange = (name: string, value: string) => {
-    setValue((prev) => ({
-      ...prev,
-      [name]: value,
-    }))
-  }
-
-  const handleSubmit = async () => {
-    UserApi.update(value)
-      .then((result) => {
-        switch (result?.status) {
-          case 200:
-            updateUser(value)
-            AndroidToast('변경되었습니다')
-            break
-          case 400:
-            AndroidToast('존재하는 닉네임입니다')
-            break
-          default:
-        }
-      })
-      .catch(() => AndroidToast('정보 변경을 실패하였습니다'))
-  }
-
-  useEffect(() => {
-    setValue({
-      nickname: nickname || '',
-      department,
-      profile_image,
-    })
-  }, [nickname, username, department, profile_image])
-
-  useEffect(() => {
-    if (!token) return
-
-    DepartmentApi.query().then(({ status, data }) => {
-      if (status !== 200) return
-
-      const newData = data.map((info: Department) => ({
-        name: info.name,
-        id: info.id,
-      }))
-      setDepartments(newData)
-    })
-  }, [token])
 
   return (
     <>
@@ -81,17 +15,7 @@ export default function MyEditPage() {
         className="header-mypage-edit"
         color="#6C6C6C"
       />
-      <Edit>
-        <Edit.Image onChange={handleChange} />
-        <Edit.Nickname value={value.nickname!} onChange={handleChange} />
-        <Edit.StudentNumber studentNumber={student_number!} />
-        <Edit.Department
-          value={value.department!}
-          departments={departments}
-          onChange={handleChange}
-        />
-        <Edit.Button onClick={handleSubmit} />
-      </Edit>
+      <Edit />
     </>
   )
 }
--- src/views/MyPage/Edit/index.tsx
+++ src/views/MyPage/Edit/index.tsx
@@ -1,4 +1,14 @@
-import { ReactNode } from 'react'
+import { useState, useEffect } from 'react'
+
+import { UserApi } from '@api/UserApi'
+import { DepartmentApi } from '@api/DepartmentApi'
+import { useToken } from '@context/Token'
+import { useUser } from '@context/User'
+import { AndroidToast } from '@utils/android'
+
+import { Department as DepartmentType } from '@interface/Department'
+import { Edit as EditType } from '@interface/User'
+
 import Button from './Button'
 import Department from './Department'
 import Image from './Image'
@@ -7,16 +17,82 @@ import StudentNumber from './StudentNumber'
 
 import * as s from './styled'
 
-interface Props {
-  children: ReactNode
+const Edit = () => {
+  const { token } = useToken()
+  const {
+    user: { username, department, nickname, student_number, profile_image },
+    updateUser,
+  } = useUser()
+
+  const [departments, setDepartments] = useState<DepartmentType[]>([])
+  const [value, setValue] = useState<EditType>({
+    nickname: nickname || '',
+    department,
+    profile_image,
+  })
+
+  const handleChange = (name: string, value: string) => {
+    setValue((prev) => ({
+      ...prev,
+      [name]: value,
+    }))
+  }
+
+  const handleSubmit = async () => {
+    UserApi.update(value)
+      .then((result) => {
+        switch (result?.status) {
+          case 200:
+            updateUser(value)
+            AndroidToast('변경되었습니다')
+            break
+          case 400:
+            AndroidToast('존재하는 닉네임입니다')
+            break
+          default:
+        }
+      })
+      .catch(() => AndroidToast('정보 변경을 실패하였습니다'))
+  }
+
+  useEffect(() => {
+    setValue({
+      nickname: nickname || '',
+      department,
+      profile_image,
+    })
+  }, [nickname, username, department, profile_image])
+
+  useEffect(() => {
+    if (!token) return
+
+    DepartmentApi.query().then(({ status, data }) => {
+      if (status !== 200) {
+        AndroidToast('학과를 불러오는데 실패하였습니다')
+        return
+      }
+
+      const newData = data.map((info: DepartmentType) => ({
+        name: info.name,
+        id: info.id,
+      }))
+      setDepartments(newData)
+    })
+  }, [token])
+
+  return (
+    <s.Container>
+      <Image onChange={handleChange} />
+      <Nickname value={value.nickname!} onChange={handleChange} />
+      <StudentNumber studentNumber={student_number!} />
+      <Department
+        value={value.department!}
+        departments={departments}
+        onChange={handleChange}
+      />
+      <Button onClick={handleSubmit} />
+    </s.Container>
+  )
 }
-
-const Edit = ({ children }: Props) => <s.Container>{children}</s.Container>
-
-Edit.Image = Image
-Edit.Nickname = Nickname
-Edit.StudentNumber = StudentNumber
-Edit.Department = Department
-Edit.Button = Button
 
 export default Edit
