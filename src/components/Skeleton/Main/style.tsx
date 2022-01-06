4c9863b87d9731f7ee2b0f1d4b4fe3aa51092e0c
Leon <leon@dunamu.com>
2022-01-06 21:33:02 +0900

    style: change main-announcement skeleton

--- src/components/Skeleton/Main/style.tsx
+++ src/components/Skeleton/Main/style.tsx
@@ -38,16 +38,39 @@ const style = {
   hotTitle: css`
     height: 17px;
     width: 70%;
+    max-width: 150px;
     ${skeleton};
   `,
   hotContent: css`
     height: 15px;
-    width: 48%;
+    width: 88%;
+    max-width: 270px;
     ${skeleton};
   `,
   hotBoardName: css`
     height: 12px;
     width: 18px;
+    ${skeleton};
+  `,
+  noticeItem: css`
+    background-color: ${color.transparency} !important;
+    box-shadow: none;
+
+    * {
+      color: ${color.transparency} !important;
+      background-color: ${color.transparency} !important;
+    }
+  `,
+  noticeTitle: css`
+    height: 17px;
+    width: 70%;
+    max-width: 150px;
+    ${skeleton};
+  `,
+  noticeContent: css`
+    height: 15px;
+    width: 88%;
+    max-width: 270px;
     ${skeleton};
   `,
 }
