45434924dc9db68ac2caf4111137d2f06aa6eab4
Leon <leon@dunamu.com>
2022-01-12 23:46:58 +0900

    feat: add caching about club, noti-setting

--- api/ClubApi.ts
+++ api/ClubApi.ts
@@ -1,8 +1,13 @@
 import { Club } from '@interface/Club'
 import { DataObj } from '@interface/Response'
+import { caching } from '@module/cache'
 import { webClient } from '@module/webClient'
 
 export const ClubApi = {
-  query: () => webClient.get<DataObj<Club[]>>(`/clubs`),
+  query: (refresh?: boolean) => {
+    const url = '/clubs'
+
+    return caching(url, () => webClient.get<DataObj<Club[]>>(url), refresh)
+  },
   categories: () => webClient.get<DataObj<string[]>>(`/club-categories`),
 }
