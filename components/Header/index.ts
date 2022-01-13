527e355c1154fb06b73a1915758f7663642a33cb
Leon <leon@dunamu.com>
2022-01-14 01:09:26 +0900

    fix: resolve build error

--- components/Header/index.ts
+++ components/Header/index.ts
@@ -1,4 +1,2 @@
 export { default as CommonHeader } from './Common'
 export { default as MainHeader } from './Main'
-export { default as NotificationsHeader } from './Notifications'
-export { default as QrcodeHeader } from './Qrcode'
