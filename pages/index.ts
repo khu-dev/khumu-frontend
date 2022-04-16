0931695b51ef3717d9215bf5921b180466a5b55c
Leon Choi <leon@dunamu.com>
2022-04-17 03:17:20 +0900

    [fix] main redirecting error

--- pages/index.ts
+++ pages/index.ts
@@ -1,13 +1,14 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 function Index() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace('/main')
+  }, [router])
+
   return null
 }
 
 export default Index
-
-export const getStaticProps = () => {
-  return {
-    redirect: {
-      destination: '/main',
-    },
-  }
-}
